63d8b9161beeaa1daffc06b8350d7c73f63a3356
danielcastilhonascimento <danielcastilho_@hotmail.com>
2024-07-20 09:29:26 -0300

    API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway

---
+++ src/test/java/me/dio/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
